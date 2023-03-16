a3d247b21f053dcfd453f67d34938db532305326
dlawhdgus <212whdgus@gmail.com>
2023-03-16 13:09:39 +0900

    add comment

--- 3/15_basic_code/basic.cpp
+++ 3/15_basic_code/basic.cpp
@@ -9,8 +9,13 @@ int add(int x, int y) {
 
 int main() {
     int a, b, sum;
+    cout << "정수를 입력해 주세요 >>  ";
     cin >> a >> b; //입력 -> c언어 기준 scanf
-    sum = add(a,b);
-    cout << sum << endl;    //cout(출력) -> printf, << : c언어 기준 "+"" 느낌, endl : c언어 기준 "\n" 한칸 내려주는 기능
-    return 0;    
+    sum = a+b;
+    cout << "합은 >>  " << sum << '\n';
+    cout << "합은 >>  " << add(a,b) << endl;    //cout(출력) -> printf, << : c언어 기준 "+"" 느낌, endl : c언어 기준 "\n" 한칸 내려주는 기능
+    cout << "전역변수 g의 값은 >>  " << g << endl;
+    int s = a != b;
+    cout << s;
+    return 0;
 }
