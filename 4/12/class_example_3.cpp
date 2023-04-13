fad0b644dbf5c2a801826b20721ebe9ea687ea15
dlawhdgus <212whdgus@gmail.com>
2023-04-13 16:17:44 +0900

    Delegating Constructor(위임 생성자)

--- 4/12/class_example_3.cpp
+++ 4/12/class_example_3.cpp
@@ -11,6 +11,12 @@ public:
 
 Circle :: Circle() : Circle(1) {};  //위임 생성자
 
+// Circle::Circle() {                                       위임 생성자를 선언하지 않았다면 Circle객체의 매개변수가 없이 넘어오는 값을 받는 생성자 함수가 없다.
+//     radius = 1;                                          매개변수 없이 넘어오는 생성자 함수를 사용하기 위해 위임 생성자를 사용한다
+//     cout << "반지름 " << radius << "인 원 생성" << endl;     위임 생성자 : 중복되는 코드를 두번 적고 싶지 않기 때문에 사용한다.
+// };
+
+
 Circle :: Circle(int r) {
     radius = r;
     cout << "반지름" << radius << "원 생성" << endl;
